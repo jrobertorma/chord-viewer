a838133bd28f094141f379b658e4c3c023b7ee34
jrobertorma <jrobertorma@gmail.com>
2021-08-19 08:20:31 -0500

    Adding react-chords library

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,4 +1,32 @@
 import * as chordsData from '../../lib/guitar.json';
+import Chord from '@tombatossals/react-chords/lib/Chord';
+
+const MyChord = () => {
+  const chord = {
+      frets: [1, 3, 3, 2, 1, 1],
+      fingers: [1, 3, 4, 2, 1, 1],
+      barres: [1],
+      capo: false,
+  }
+  const instrument = {
+      strings: 6,
+      fretsOnChord: 4,
+      name: 'Guitar',
+      keys: [],
+      tunings: {
+          standard: ['E', 'A', 'D', 'G', 'B', 'E']
+      }
+  }
+  const lite = false // defaults to false if omitted
+  return (
+      <Chord
+          chord={chord}
+          instrument={instrument}
+          lite={lite}
+          
+      />
+  )
+}
 
 function App() {
   const cmaj = chordsData.chords.C[0].positions[0].frets.map( (value) => {
@@ -12,6 +40,11 @@ function App() {
       <ul>
         {cmaj}
       </ul>
+    <div style={{width: "200px"}}>
+      <MyChord />
+    </div>
+      
+      
     </div>
   );
 }
