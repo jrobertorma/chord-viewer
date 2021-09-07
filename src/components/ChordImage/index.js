f15894afdfa83fdeb58e357da7691fe95a2c6dbc
jrobertorma <jrobertorma@gmail.com>
2021-09-07 08:57:07 -0500

    Using oscillator to reproduce notes

--- src/components/ChordImage/index.js
+++ src/components/ChordImage/index.js
@@ -2,57 +2,11 @@ import React from 'react';
 import Chord from '@tombatossals/react-chords/lib/Chord';
 
 import Typography from '@material-ui/core/Typography';
-
-const ChordDiagram = ({chordPositions}) => {
-    
-    const chord = {
-        frets: chordPositions.frets,
-        fingers: chordPositions.fingers,
-        barres: chordPositions.barres,
-        capo: chordPositions.capo ? chordPositions.capo : false,
-        baseFret: chordPositions.baseFret,
-    }
-    const instrument = {
-        strings: 6,
-        fretsOnChord: 4,
-        name: 'Guitar',
-        keys: [],
-        tunings: {
-            standard: ['E', 'A', 'D', 'G', 'B', 'E']
-        }
-    }
-    const lite = false // defaults to false if omitted
-    return (
-        <Chord
-            chord={chord}
-            instrument={instrument}
-            lite={lite}
-        />
-    )
-  }
-
-const ChordImage = (props) => {
-    const chordData = props.chordData;
-    const position = props.position;
-
-    return ( 
-        <div style={{'minWidth':'144px'}}> 
-            <Typography variant="h6" gutterBottom>
-                {chordData.key+" "+chordData.suffix}
-            </Typography>
-            <ChordDiagram chordPositions={chordData.positions[position]}/>
-        </div>);
-}
- 
-export default ChordImage;
-
-/**
- * Agregamos una 'clickable surface' en el contenedor?
- * en el listener del onClick le ponemos el generador MIDI?
- */
+import CardActionArea from '@material-ui/core/CardActionArea';
 
 var context = null;
 var oscillator = null;
+
 function getOrCreateContext() {
   if (!context) {
     context = new AudioContext();
@@ -64,6 +18,7 @@ function getOrCreateContext() {
 }
 
 let isStarted = false;
+
 function noteOn(midiNote) {
     getOrCreateContext();
     const freq = Math.pow(2, (midiNote-69)/12)*440;
@@ -80,5 +35,54 @@ function noteOff() {
     context.suspend();
 }
 
-noteOn(key);
-noteOff();
+const onChordClick = (midiData) => {
+    console.log('yoyoyo '+ midiData[0]);
+    noteOn(midiData[0]);
+    setTimeout(noteOff(), 3000);
+}
+
+const ChordDiagram = ({chordPositions}) => {
+    const chord = {
+        frets: chordPositions.frets,
+        fingers: chordPositions.fingers,
+        barres: chordPositions.barres,
+        capo: chordPositions.capo ? chordPositions.capo : false,
+        baseFret: chordPositions.baseFret,
+    }
+
+    const instrument = {
+        strings: 6,
+        fretsOnChord: 4,
+        name: 'Guitar',
+        keys: [],
+        tunings: {
+            standard: ['E', 'A', 'D', 'G', 'B', 'E']
+        }
+    }
+
+    const lite = false // defaults to false if omitted
+
+    return (
+        <Chord
+            chord={chord}
+            instrument={instrument}
+            lite={lite}
+        />
+    )
+  }
+
+const ChordImage = (props) => {
+    const chordData = props.chordData;
+    const position = props.position;
+    
+    return ( 
+        <CardActionArea style={{'minWidth':'144px'}} onClick={ () => onChordClick(chordData.positions[position].midi) }>
+            <Typography variant="h6" gutterBottom>
+                {chordData.key+" "+chordData.suffix}
+            </Typography>
+            <ChordDiagram chordPositions={chordData.positions[position]}/>
+        </CardActionArea>
+    );
+}
+ 
+export default ChordImage;
